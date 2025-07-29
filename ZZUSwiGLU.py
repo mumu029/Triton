78e00a1c354c6e0fea5f07595a77a3f20a98e50e
沐目 <51202355+mumu029@users.noreply.github.com>
2025-07-29 23:32:05 +0800

    Update ZZUSwiGLU.py

--- ZZUSwiGLU.py
+++ ZZUSwiGLU.py
@@ -145,7 +145,6 @@ class ZZUSwiGLU(nn.Module):
         self.intermediate_size = config.intermediate_size
         self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
         self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        
         self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
         self.act_fn = _silu
 
